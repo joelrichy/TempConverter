b4317adefd7c401c0157033d91c264687344a1a5
joelrichy <richardson@middleton.school.nz>
2022-03-17 14:42:46 +1300

    17/3/22

---
+++ 01_Help_GUIv1.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import random
+
+
+class Convertor:
+    def __init__(self):
+        print("Hello world")
+
+        #formatting variables...
+        background_colour = "light blue"
+
+        # Converter Main Screen GUI
+        self.converter_frame = Frame(width=300, height=300, bg=background_colour)
+        self.converter_frame.grid()
+
+        # Tempurature Conversion Heading (row 0)
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          font=("Arial", 16, "bold"),
+                                          bg=background_colour,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Tempurature Convertor")
+    something = Convertor()
+    root.mainloop()
---
+++ HELP GUI.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import random
+
+
+class Convertor:
+    def __init__(self, parent):
+        print("Hello world")
+
+
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Tempurature Convertor")
+    something = Convertor(root)
+    root.mainlooop()
